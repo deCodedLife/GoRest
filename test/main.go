94d48781f03f9c7f0664adfdb0896db801de313e
deCodedLife <dcodedlife@gmail.com>
2022-10-03 14:57:09 +0300

    Switch to package

--- test/main.go
+++ test/main.go
@@ -1,7 +1,21 @@
 package main
 
-import . "github.com/deCodedLife/gorest/rest"
+import (
+	. "github.com/deCodedLife/gorest/rest"
+	. "github.com/deCodedLife/gorest/tool"
+	"github.com/gorilla/mux"
+	"net/http"
+)
 
 func main() {
-	Construct()
+	Handlers := Construct()
+
+	r := mux.NewRouter()
+
+	for _, api := range Handlers {
+		r.HandleFunc("/"+api.Path, api.Handler).Methods(api.Method)
+	}
+
+	err := http.ListenAndServe(":80", r)
+	HandleError(err, CustomError{}.Unexpected(err))
 }
